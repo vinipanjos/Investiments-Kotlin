91aa2ffd90b30e47874c054c9d1ccbcae05094bf
vinipanjos <vinipanjos@hotmail.com>
2022-02-17 15:44:30 -0300

    changing and optimizing code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,43 @@
+import java.awt.List
 import java.util.*
 
 fun main(args: Array<String>) {
+    //val wallets = arrayListOf<String>()
 
-    var validate = true
-    do {
+    for (i in 0 until 3) {
         println("Digite o nome, preço e quantidade da ação que deseja comprar, ou digite 0 para sair")
         val nome = readLine()
         val preco = readLine()
         val quant = readLine()
-        if (nome.equals("0") && preco.equals("0") && quant.equals("0")) {
-            !validate
-            println("Aplicação sendo encerrada.")
-        } else {
-            val wallets = arrayListOf<String>()
-            wallets.add(Wallet(nome, preco, quant).toString())
-            }
-    } while (!validate)
 
+        val wallet: Wallet = Wallet(nome, preco, quant)
+
+        if (nome.equals("0") || preco.equals("0") || quant.equals("0")) {
+            println("Aplicação sendo encerrada.")
+
+        } else {
+            Wallet(nome, preco, quant).addWallet(wallet) //= println(Wallet(nome, preco, quant).toString())
+            Wallet(nome, preco, quant).toString()
+        }
+        println()
+    }
 }
 
-fun Wallet(name: String?, price: String?, amount: String?) {
+class Wallet(var name: String?, var price: String?, var amount: String?) {
+    private val allWallets = mutableListOf<Wallet>()
+
     val priced: Double = price?.toDouble()!!
     val amounted: Double = amount?.toDouble()!!
 
     val totalInvest: Double = amounted * priced
-    val toString = println(
-        "Nome da ação: " + name + "\nPreço: " + price + "\nQuantidade: " + amount +
-                "\nPreço médio:" + (totalInvest / amounted) + "\nValor total do investimento: " + totalInvest
-    )
+
+    fun addWallet(w: Wallet) {
+        allWallets.add(w)
+    }
+    override fun toString(): String {
+        return println("Nome da ação é " + name + ", avaliado em R$ " + price + ", comprou a quantidade de " + amount +
+                " unidadess, com um preço médio de R$" + (totalInvest / amounted) + ". Valor total do investimento: R$" + totalInvest).toString()
+    }
 }
 
 
