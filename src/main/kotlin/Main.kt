f40fba87898669ddfb49c4a67a85b3a77c7e56a3
Vinicius <vinipanjos@hotmail.com>
2022-02-19 22:07:41 -0300

    Implementando o conceito de conjuntos, utilizando for, arrays e ifelse

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,43 +1,83 @@
-import java.awt.List
-import java.util.*
+import kotlin.io.println as println
 
 fun main(args: Array<String>) {
-    //val wallets = arrayListOf<String>()
 
-    for (i in 0 until 3) {
-        println("Digite o nome, preço e quantidade da ação que deseja comprar, ou digite 0 para sair")
-        val nome = readLine()
-        val preco = readLine()
-        val quant = readLine()
+    val wallet = mutableListOf<Wallet>()
+    val name = mutableListOf<String>()
+    val price = mutableListOf<Double>()
+    val amount = mutableListOf<Double>()
 
-        val wallet: Wallet = Wallet(nome, preco, quant)
+    name.add("Oi")
+    price.add(25.0)
+    amount.add(20.0)
 
-        if (nome.equals("0") || preco.equals("0") || quant.equals("0")) {
-            println("Aplicação sendo encerrada.")
+    name.add("Tim")
+    price.add(30.0)
+    amount.add(20.0)
 
-        } else {
-            Wallet(nome, preco, quant).addWallet(wallet) //= println(Wallet(nome, preco, quant).toString())
-            Wallet(nome, preco, quant).toString()
+    name.add("Vivo")
+    price.add(25.0)
+    amount.add(20.0)
+
+    name.add("Claro")
+    price.add(100.0)
+    amount.add(20.0)
+
+    name.add("Oi")
+    price.add(20.0)
+    amount.add(20.0)
+
+    name.add("Claro")
+    price.add(45.0)
+    amount.add(20.0)
+
+    name.add("Vivo")
+    price.add(30.0)
+    amount.add(20.0)
+
+    name.add("Tim")
+    price.add(10.0)
+    amount.add(20.0)
+
+    name.add("Tim")
+    price.add(10.0)
+    amount.add(20.0)
+
+    name.add("Tim")
+    price.add(10.0)
+    amount.add(20.0)
+
+    name.add("Tim")
+    price.add(10.0)
+    amount.add(20.0)
+
+    for (i in 0 until price.size) {
+        val nome = name[i]
+        val preco = price[i]
+        val quant = amount[i]
+        wallet.add(Wallet(nome, preco, quant))
+        for (j in 0 until i) {
+            if (nome == name[j]) {
+                wallet[j].amount += quant
+                wallet[j].price = (preco + wallet[j].price) / 2
+                wallet[i] = Wallet("", 0.0, 0.0)
+            }
         }
-        println()
+    }
+    for (i in 0 until name.size) {
+        if (wallet[i].name != "")
+            wallet[i].toString()
     }
 }
 
-class Wallet(var name: String?, var price: String?, var amount: String?) {
-    private val allWallets = mutableListOf<Wallet>()
+class Wallet(val name: String, var price: Double, var amount: Double) {
 
-    val priced: Double = price?.toDouble()!!
-    val amounted: Double = amount?.toDouble()!!
-
-    val totalInvest: Double = amounted * priced
-
-    fun addWallet(w: Wallet) {
-        allWallets.add(w)
-    }
-    override fun toString(): String {
-        return println("Nome da ação é " + name + ", avaliado em R$ " + price + ", comprou a quantidade de " + amount +
-                " unidadess, com um preço médio de R$" + (totalInvest / amounted) + ". Valor total do investimento: R$" + totalInvest).toString()
-    }
+    override fun toString(): String = println(
+        "Nome da ação é " + name + ", comprou " + amount +
+                " unidadess, com um preço médio de R$" + ((amount * price) / amount) + ". Valor total do investimento: R$" + (amount * price)
+    ).toString()
 }
+
+
 
 
